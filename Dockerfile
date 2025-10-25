b04f05e003e40a6e68c9ff0c9361b284973fb3f3
Vulbough <71969296+riszkyhermawan@users.noreply.github.com>
2025-10-25 22:56:21 +0700

    (feat) add docker file

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,4 @@ RUN pip install -r /app/requirements.txt
 
 COPY . /app/
 
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8001"]
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "9001"]
--- main.py
+++ main.py
@@ -20,14 +20,18 @@ class Code(BaseModel):
 @app.post("/run")
 async def run_code(code: Code):
     user_code = code.source_code
-    user_input = code.input_data or ""
+    user_input = code.input_data or None
     
     try:
         if user_input == None:
-            run_code = f"echo \"{user_input}\" | python -c \"{user_code}\""
-        else:
             run_code = f"python -c \"{user_code}\""
-        output = client.containers.run("python", run_code)
+        else:
+            run_code = f"echo \"{user_input}\" | python -c \"{user_code}\""
+        output = client.containers.run(
+            "python", 
+            f"sh -c '{run_code}'",
+            remove=True
+        )
         return {"output": output.decode("utf-8").strip()}
     except docker.errors.ContainerError as e: # type: ignore
         return {"error": e.stderr.decode("utf-8").strip()}
