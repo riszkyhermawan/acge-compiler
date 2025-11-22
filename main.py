e9a0db2a80adfb173e24faa978efc4402d986289
Vulbough <71969296+riszkyhermawan@users.noreply.github.com>
2025-11-22 21:40:16 +0700

    - (fix) handle quotes in code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import docker
+import shlex
 
 app = FastAPI(title="Compiler API")
 
@@ -14,22 +15,25 @@ async def read_root():
 
 class Code(BaseModel):
     source_code: str
-    input_data: str | None = None
-    
+    input_data: str | None = None 
+        
     
 @app.post("/run")
 async def run_code(code: Code):
     user_code = code.source_code
     user_input = code.input_data or None
     
+    escaped_code = shlex.quote(user_code)
+    
     try:
-        if user_input == None:
-            run_code = f"python -c \"{user_code}\""
+        if user_input:
+            input_escaped = shlex.quote(user_input)
+            run_code = f"echo {input_escaped} | python -u -c {escaped_code}"
         else:
-            run_code = f"echo \"{user_input}\" | python -c \"{user_code}\""
+            run_code = f"python -u -c {escaped_code}"
         output = client.containers.run(
-            "python", 
-            f"sh -c '{run_code}'",
+            "python:3.11-slim", 
+            ["/bin/sh", "-c", run_code],
             remove=True
         )
         return {"output": output.decode("utf-8").strip()}
